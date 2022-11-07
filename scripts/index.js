41132527fbee61c3185e1c3e3fb928462a618e5c
Zachary Skiles <skilesz@purdue.edu>
2022-11-06 19:41:16 -0500

    Save to scoreboard capabilities

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,7 @@ const months = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July',
 
 // GLOBAL
 let days = 0;
+let calendarDay = 0;
 
 
 // EVENT LISTENERS
@@ -19,7 +20,7 @@ world.events.beforeItemUse.subscribe((eventData) => {
         case 'calendar:calendar':
             let calendarForm = new MessageFormData();
             calendarForm.title('§l§6Today\'s Date');
-            calendarForm.body('Today is §l§b' + months[Math.floor(days / 30)] + ' ' + ((days % 30) + 1) + '§r!');
+            calendarForm.body('Today is §l§b' + months[Math.floor(calendarDay / 30)] + ' ' + ((calendarDay % 30) + 1) + '§r!');
             calendarForm.button1('Ok');
             calendarForm.button2('Close');
 
@@ -43,10 +44,32 @@ world.events.beforeItemUse.subscribe((eventData) => {
 
 // ON-TICK
 world.events.tick.subscribe(eventData => {
+    // First tick
+    if (eventData.currentTick === 0) {
+        // Setup scoreboard if not already done
+        if (!world.scoreboard.getObjectives().includes('calendar')) {
+            world.scoreboard.addObjective('calendar');
+            world.getDimension('minecraft:overworld').runCommand('scoreboard players set days calendar 0');
+        }
+    }
+
     // Every Minecraft day
-    if (eventData.currentTick % 24000 === 0) {
+    if (eventData.currentTick % 100 === 0) {
+        // Read from scoreboard
+        var parts = world.scoreboard.getObjective('calendar').getParticipants();
+        parts.forEach(part => {
+            if (part.displayName === 'days') {
+                days = world.scoreboard.getObjective('calendar').getScore(part);
+            }
+        });
+
+        calendarDay = days;
+
         world.say('§6§l[CALENDAR]§r: Today is §b§l' + months[Math.floor(days / 30)] + ' ' + ((days % 30) + 1) + '§r!');
         days++;
         if (days === 360) days = 0;
+        
+        // Write to scoreboard
+        world.getDimension('minecraft:overworld').runCommand('scoreboard players set days calendar ' + days);
     }
 });
